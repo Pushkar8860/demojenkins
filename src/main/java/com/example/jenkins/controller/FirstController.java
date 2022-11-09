1a2585504cdbc4d2406bc19230b9fe4800170d59
pushkar8389 <49140942+pushkar8389@users.noreply.github.com>
2022-11-09 09:12:12 +0530

    initial first commit

---
+++ src/main/java/com/example/jenkins/controller/FirstController.java
@@ -0,0 +1,17 @@
+package com.example.jenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstController {
+    @GetMapping("/test")
+    public String test() {
+        return "Welcome to test Mapping";
+    }
+
+    @GetMapping("/world")
+    public String world() {
+        return "Welcome to world Mapping";
+    }
+}
